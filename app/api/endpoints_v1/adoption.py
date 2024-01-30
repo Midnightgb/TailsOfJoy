9dee687030c4948f80e1b575bee113f97339702a
Chocorramin <danilq139@gmail.com>
2024-01-30 18:52:56 -0500

    Add adoption status to Pet model and update adopt_pet endpoint

--- app/api/endpoints_v1/adoption.py
+++ app/api/endpoints_v1/adoption.py
@@ -3,12 +3,12 @@ from logger.Logger import Logger
 from db.database import *
 from fastapi import Depends
 from sqlalchemy.orm import Session
-from models.pets import Pet
+from models.pets import Pet, Status
 
 router = APIRouter()
 
 
-@router.get("/list")
+""" @router.get("/list")
 def get_available_pets(
     db: Session = Depends(get_database),
 ):
@@ -20,7 +20,20 @@ def get_available_pets(
     Logger.success("Pet list retrieved")
     available_pets = db.query(Pet).all()
     return {"pets": available_pets}
-
+"""
 @router.post("/adopt/{pet_id}")
-def adopt_pet(pet_id: int):
-    return {"message": f"Pet with ID {pet_id} adopted successfully"}
+def adopt_pet(pet_id: int, db: Session = Depends(get_database)):
+    if not serverStatus(db):
+        return {
+            "status": "false",
+            "message": "Database server is down. Please try again later."
+        }
+    Logger.success("Pet list retrieved")
+    pet_to_adopt = db.query(Pet).filter(Pet.pet_id == pet_id).first()
+    Logger.info(pet_to_adopt)
+    if not pet_to_adopt:
+        return {"message": f"Pet with ID {pet_id} not found"}
+    #if pet_to_adopt.adopted:
+        #return {"message": f"Pet with ID {pet_id} already adopted"}
+    
+    
