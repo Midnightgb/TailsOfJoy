74822d2fcf901a5a3ec0699ead5b08fcf0badd28
midnightgb <midnight3424@gmail.com>
2024-06-12 08:53:19 -0500

    refactor: Update project settings and add API description

--- Back - Python/core/config.py
+++ Back - Python/core/config.py
@@ -10,6 +10,10 @@ load_dotenv(dotenv_path=env_path)
 
 
 class Settings(BaseSettings):
+    PROJECT_NAME: str = "Tails of Joy API"
+    PROJECT_VERSION: str = "0.0.1"
+    PROJECT_DESCRIPTION: str = "API for Tails of Joy an application for pet adoption"
+    
     DB_USER: str = os.getenv("DB_USER")
     DB_PASS: str = os.getenv("DB_PASS")
     DB_NAME: str = os.getenv("DB_NAME")
@@ -18,6 +22,9 @@ class Settings(BaseSettings):
     DATABASE_URL: str = f"postgresql+psycopg2://{DB_USER}:%s@{DB_HOST}:{DB_PORT}/{DB_NAME}" % quote_plus(
         DB_PASS)
 
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    TOKEN_EXPIRE_MINUTES = 30
+    ALGORITHM: str = os.getenv("ALGORITHM")
 
 def get_settings() -> Settings:
     return Settings()
