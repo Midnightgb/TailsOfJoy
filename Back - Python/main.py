2ae2c368ed957e48622d447e277bda408361c5b7
midnightgb <midnight3424@gmail.com>
2024-06-12 10:00:38 -0500

    refactor: Add user_router to main.py

--- Back - Python/main.py
+++ Back - Python/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from core.Logger import Logger
 #from api.v1.routes import pets
 from api.routes import health_checker
+from api.routes import user_router
 
 Logger.info("Starting API")
 
@@ -15,3 +16,4 @@ def root():
 
 #app.include_router(pets.router)
 app.include_router(health_checker.router)
+app.include_router(user_router.router)
