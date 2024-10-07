da17a2a6d047e19c68b8c9e9df4e8a049ee0787b
Midnightgb <midnight3424@gmail.com>
2024-10-07 18:59:54 -0500

    feat:

--- app/api/models/pets/specie.py
+++ app/api/models/pets/specie.py
@@ -6,10 +6,10 @@ from api.models.pets.breed import Breed
 class Specie(Base):
     __tablename__ = 'species'
 
-    specie_id = Column(String(255), primary_key=True)
+    species_id = Column(String(255), primary_key=True)
     breed_id = Column(String(255), ForeignKey('breeds.breed_id'), nullable=False)
     name = Column(String(255))
-    specie_description = Column(String(255))
+    species_description = Column(String(255))
     created_at = Column(TIMESTAMP, nullable=False, server_default='CURRENT_TIMESTAMP')
     updated_at = Column(TIMESTAMP, nullable=False, server_default='CURRENT_TIMESTAMP', onupdate='CURRENT_TIMESTAMP')
     
--- app/api/schemas/pets/specie.py
+++ app/api/schemas/pets/specie.py
@@ -2,10 +2,10 @@ from pydantic import BaseModel
 
 class Specie(BaseModel):
     name : str
-    specie_description : str 
+    species_description : str 
 
 class Specie_create(Specie):
     breed_id : str
 
 class Specie_read(BaseModel):
-    specie_id : str
+    species_id : str
